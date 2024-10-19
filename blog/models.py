5144a5819ed90c2d4760bf279df31cacd5221b21
Cam-4587 Conroy <cameronconroy@live.com>
2024-10-19 15:40:01 +0000

    comment model added

--- blog/models.py
+++ blog/models.py
@@ -22,4 +22,17 @@ class Post(models.Model):
 
        def __str__(self):
         return f"{self.title} | {self.author}"
+
+class Comment(models.Model):
+       post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comments')
+       author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+       content= models.TextField()
+       approved = models.BooleanField(default=False)
+       created_on = models.DateTimeField(auto_now_add=True)
+       
+       class Meta:
+        ordering = ["-created_on"]
+        
+       def __str__(self):
+         return f"{self.author} | {self.post}"
        
