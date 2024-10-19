0df819722b7aba39e2e8e8477dd80b8da54346e8
Cam-4587 Conroy <cameronconroy@live.com>
2024-10-19 19:33:20 +0000

    Add Tag model, TagAdmin model, CommentItemInline model, PostAdmin model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 
 # Register your models here.
-admin.site.register(Post)
+
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    prepopulated_fields = {'slug': ('tag',)}
+    
+class CommentItemInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+    
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    inlines = [CommentItemInline]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
+admin.site.register(Tag,TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 STATUS = ((0, "Pending Review"), (1, "Published"))
 
+class Tag(models.Model):
+       tag = models.CharField(max_length=20, unique=True)
+       slug = models.SlugField(max_length=20, unique=True)
+       
+       def __str__(self):
+        return f"{self.tag}"
+
 class Post(models.Model):
        title = models.CharField(max_length=200, unique=True)
        slug = models.SlugField(max_length=200, unique=True)
+       tags = models.ManyToManyField(Tag)
        author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts") 
        image = models.ImageField(upload_to='upload/', blank=True, null=True)
        intro = models.TextField(blank=True)
