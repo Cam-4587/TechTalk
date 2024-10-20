424da11ba1349e1bcb7100d79d1b4cd41a9823a7
Cam-4587 Conroy <cameronconroy@live.com>
2024-10-20 11:43:43 +0000

    Add Reply model, CommentAdmin model and  ReplyItemInline model

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from .models import Post, Comment, Tag
+from .models import Post, Comment, Tag, Reply
 
 # Register your models here.
 
+class CommentItemInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+class ReplyItemInline(admin.TabularInline):
+    model = Reply
+    extra = 1
 class TagAdmin(admin.ModelAdmin):
     model = Tag
     prepopulated_fields = {'slug': ('tag',)}
+    
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+    inlines = [ReplyItemInline]
     
 class CommentItemInline(admin.TabularInline):
     model = Comment
@@ -19,5 +29,6 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Reply)
 admin.site.register(Tag,TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -43,4 +43,14 @@ class Comment(models.Model):
         
        def __str__(self):
          return f"{self.author} | {self.post}"
+  
+class Reply(models.Model):
+       reply = models.ForeignKey(Comment, on_delete=models.CASCADE,related_name='reply')
+       author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="replier")
+       content= models.TextField()
+       approved = models.BooleanField(default=False)
+       created_on = models.DateTimeField(auto_now_add=True)
+       
+       def __str__(self):
+         return f"{self.author} | {self.reply}"
        
