7ed1cf4b8cb3aa3dbae078aa39f9b0e848bcd1b8
Cam-4587 Conroy <cameronconroy@live.com>
2024-10-22 13:00:35 +0000

    Create view and html display for showing blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
+from django.shortcuts import render, get_object_or_404, reverse
 from .models import Post 
-
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.all()
     template_name = "blog/index.html"
     paginate_by = 6  
+    
+
+
+def post_blog(request, slug):
+    queryset = Post.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_blog.html",
+        {"post": post},
+    )
