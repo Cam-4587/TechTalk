77ebc1e95ced5ba02b65bf5e4ea06fca56c0dae8
Cam-4587 Conroy <cameronconroy@live.com>
2024-11-02 17:33:29 +0000

    Configure Profile.html page to either show User Profile or User profile form

--- about/views.py
+++ about/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from .models import Profile
+from.models import Profile
+from .forms import UpdateProfileForm
 # Create your views here.
 def profile(request):
     """
     Renders the Profile page
     """
     profile = Profile.objects.all().order_by('updated_on').first
+    profile_form = UpdateProfileForm()
     
     return render(
     request,
     "about/profile.html",
-    {"profile": profile},
+    {"profile": profile,
+    "profile_form": profile_form},
 )
