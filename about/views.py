92d96618c871b8f5abab24d45fdfbfc07491c4d7
Cam-4587 Conroy <cameronconroy@live.com>
2025-03-10 20:33:49 +0000

    fix: Remove login required decorators and further user authentication to views editing and deleting profiles

--- about/views.py
+++ about/views.py
@@ -51,18 +51,13 @@ def profile(request):
 
     return render(request, "about/profile.html", context)
 
-@login_required
+
 def UpdateProfile(request):
     """
     Updates the users Profile
     """
     profile, created = Profile.objects.get_or_create(user=request.user)
     profile_form = UpdateProfileForm(instance=profile)
-    
-    # Check if the current user is the author of the post
-    if post.author != request.user:
-        messages.error(request, "You do not have permission to edit this profile.")
-        return redirect('home')
 
     if request.method == "POST":
         profile_form = UpdateProfileForm(
@@ -83,17 +78,12 @@ def UpdateProfile(request):
 
     return render(request, "about/profile_edit.html", context)
 
-@login_required
+
 def DeleteProfile(request):
     """
     Deletes the Users Profile
     """
     profile, created = Profile.objects.get_or_create(user=request.user)
-    
-     # Check if the current user is the author of the post
-    if post.author != request.user:
-        messages.error(request, "You do not have permission to delete this profile.")
-        return redirect('home')
 
     if request.method == "POST":
         profile.delete()
